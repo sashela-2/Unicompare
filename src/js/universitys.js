33f231cd20e8d321eb905205b5d64955112716d8
sashela-2 <20246@jpc.school.nz>
2026-09-01 09:12:15 +1200

    FIXED CARROSEL
    - I though i had fixed this issue long ago but it came back up during testing this the fixed code becuase in the indexed [0} did not actually start at 0 and broke the images

--- src/js/universitys.js
+++ src/js/universitys.js
@@ -1,12 +1,12 @@
 const images = [
-  "src/Images/Waikato-Photos/University_of_Waikato_Official_Opening_1965.jpg",
-  "src/Images/Waikato-Photos/University_of_Waikato_Official_Opening_1965.jpg",
   "src/Images/Waikato-Photos/Waikato-Student-Centre.png",
   "src/Images/Waikato-Photos/University_of_Waikato_Official_Opening_1965.jpg"
 ];
 
 let current = 0;
 const img = document.getElementById("carouselImage");
+
+img.src = images[current]; // <-- set it immediately on load
 
 document.getElementById("next").addEventListener("click", () => {
   current = (current + 1) % images.length;
